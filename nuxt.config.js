a3b4dcedd9aab26f9de0bcb27f0227551e79d876
Kotolav <kotolav@gmail.com>
2021-11-08 01:47:42 +0400

    add plugins loading to config

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,10 @@ export default {
    },
 
    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-   plugins: [],
+   plugins: [
+      '~/plugins/languageNameFormatter.ts',
+      '~/plugins/dateDiffHuman.ts',
+   ],
 
    // Auto import components: https://go.nuxtjs.dev/config-components
    components: true,
