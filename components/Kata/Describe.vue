5bd99b584132fe75f0e892970bda910a87e345db
Kotolav <kotolav@gmail.com>
2021-11-07 21:18:05 +0400

    add kata-describe component

---
+++ components/Kata/Describe.vue
@@ -0,0 +1,23 @@
+<template>
+   <div class="description">{{ description }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+   props: {
+      description: {
+         type: String,
+         required: true,
+      },
+   },
+})
+</script>
+
+<style lang="scss">
+.description {
+   font-size: 16px;
+   color: $default_text;
+}
+</style>
