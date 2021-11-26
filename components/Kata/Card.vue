247b172a02c81fd1d2f1dbb328141e6211161ede
Kotolav <kotolav@gmail.com>
2021-11-27 01:19:51 +0400

    add kata-describe and kata-actions-panel

--- components/Kata/Card.vue
+++ components/Kata/Card.vue
@@ -1,25 +1,54 @@
 <template>
    <article class="kata">
-      <kata-header :title="data.title" :rank="data.rank" :tags="data.tags" />
+      <kata-header :name="data.name" :rank="data.rank" :tags="data.tags" />
       <div class="kata__content">
-         <kata-describe :description="data.description" />
+         <kata-describe
+            :description="data.description"
+            :used-languages="solutionLanguages"
+         />
+         <kata-actions
+            :is-visible-solutions="isVisibleSolutions"
+            :kata-id="data.id"
+         />
          <hr class="kata__hr" />
-         <kata-solutions-list />
+         <kata-solutions-list :solutions="data.solutions" />
       </div>
       <kata-footer></kata-footer>
    </article>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
-import { Kata } from '~/types/types'
+import { computed, defineComponent, provide } from '@nuxtjs/composition-api'
+import { EditKata } from '~/types/types'
 
 export default defineComponent({
    props: {
       data: {
-         type: Object as () => Kata,
+         type: Object as () => EditKata,
          required: true,
       },
+   },
+
+   setup(props) {
+      provide('kataId', props.data.id)
+
+      const visibleSolutions = computed(() => {
+         return props.data.solutions.filter((solution) => solution.isShowing)
+      })
+
+      const solutionLanguages = computed<string[]>(() => [
+         ...new Set(props.data.solutions.map((s) => s.language)),
+      ])
+
+      const isVisibleSolutions = computed(
+         () => visibleSolutions.value.length > 0
+      )
+
+      return {
+         visibleSolutions,
+         solutionLanguages,
+         isVisibleSolutions,
+      }
    },
 })
 </script>
@@ -27,16 +56,16 @@ export default defineComponent({
 <style scoped lang="scss">
 .kata {
    padding: 20px;
-   margin: 20px;
    border-radius: 5px;
    background-color: #262729;
    color: $default_text;
-   font-family: 'DM Sans', sans-serif;
+   font-family: 'Lato', sans-serif;
    line-height: 1.5;
+   box-shadow: 0 1px 12px 3px rgba(0, 0, 0, 0.92);
+   border: 1px solid #222;
 
    &__content {
       margin-top: 15px;
-      margin-bottom: 15px;
    }
 
    &__hr {
