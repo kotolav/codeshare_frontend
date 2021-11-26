5117527eb619f09aa2810fd23f1b89ad5a432057
Kotolav <kotolav@gmail.com>
2021-11-27 01:18:55 +0400

    add kata-solution-list

---
+++ components/Kata/SolutionsList.vue
@@ -0,0 +1,59 @@
+<template>
+   <div class="solution-list">
+      <div
+         v-for="(solutionsList, language) in langSolutions"
+         :key="language"
+         class="language"
+      >
+         <h4 class="language__title">{{ language | languageFormat }}</h4>
+         <kata-solution
+            v-for="(solution, i) in solutionsList"
+            :key="solution.id"
+            :solution="solution"
+            :variant-number="solutionsList.length > 1 ? i + 1 : null"
+            class="solution-list__item"
+         />
+      </div>
+   </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from '@nuxtjs/composition-api'
+import groupBy from 'lodash/groupBy'
+import { EditSolution } from '~/types/types'
+
+export default defineComponent({
+   props: {
+      solutions: {
+         type: Array as PropType<EditSolution[]>,
+         required: true,
+      },
+   },
+
+   setup(props) {
+      const langSolutions = computed(() =>
+         groupBy(props.solutions, (s: EditSolution) => s.language)
+      )
+
+      return {
+         langSolutions,
+      }
+   },
+})
+</script>
+
+<style lang="scss" scoped>
+.solution-list {
+   &__item:not(:last-child) {
+      margin-bottom: 16px;
+   }
+}
+
+.language {
+   &__title {
+      color: $default_text;
+      font-size: 15px;
+      margin: 1.5em 0 0.25em;
+   }
+}
+</style>
