dda5cf6be37147ef984650def35e271dc0abf3cd
Kotolav <kotolav@gmail.com>
2021-12-10 03:57:38 +0400

    change page default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,27 @@
 <template>
-   <div>
+   <div class="page">
       <Nuxt />
    </div>
 </template>
 
-<style lang="scss"></style>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+   setup() {},
+})
+</script>
+
+<style lang="scss" scoped>
+.page {
+   background-color: #4158d0;
+   background-image: linear-gradient(
+      43deg,
+      #4158d0 0%,
+      #c850c0 46%,
+      #ffcc70 100%
+   );
+   background-attachment: fixed;
+   min-height: 100vh;
+}
+</style>
